60db0816088062959be5a31284fa9a7f0d68b30a
Frédéric Rouxel <fredericrouxel@orange.fr>
2023-02-28 00:12:36 +0100

    installation mongoose

--- backend/app.js
+++ backend/app.js
@@ -2,6 +2,30 @@ const express = require('express');
 
 const app = express();
 
+const mongoose = require('mongoose');
+
+app.use(express.json());
+
+mongoose.connect('mongodb+srv://Fredev:Doudoune22@clusterpiiquante.dlumvdo.mongodb.net/?retryWrites=true&w=majority',
+  { useNewUrlParser: true,
+    useUnifiedTopology: true })
+  .then(() => console.log('Connexion à MongoDB réussie !'))
+  .catch(() => console.log('Connexion à MongoDB échouée !'));
+
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+  next();
+});
+
+app.post('/api/sauces', (req, res, next) => {
+  console.log(req.body);
+  res.status(201).json({
+    message: 'Sauce créée !'
+  });
+});
+
 app.use((req, res, next) => {
   console.log('Requête reçue !');
   next();
